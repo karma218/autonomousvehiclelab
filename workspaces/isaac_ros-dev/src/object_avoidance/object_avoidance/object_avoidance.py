5c3e6989bf5dd74a94593a6189941af50933a527
Abhishek Vishwakarma <avishwakarma@cpp.edu>
2024-09-06 13:13:37 -0700

    add stages

--- workspaces/isaac_ros-dev/src/object_avoidance/object_avoidance/object_avoidance.py
+++ workspaces/isaac_ros-dev/src/object_avoidance/object_avoidance/object_avoidance.py
@@ -7,199 +7,6 @@ import cv2
 from matplotlib import pyplot as plt
 import glob
 
-
-cnt = 0
-
-def box_label(image, box, b1, b2, label='', color=(128, 128, 128), txt_color=(255, 255, 255)):
-  lw = max(round(sum(image.shape) / 2 * 0.003), 2)
-  p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
-  b1.append(p1)
-  b2.append(p2)
-  cv2.rectangle(image, p1, p2, color, thickness=lw, lineType=cv2.LINE_AA)
-
-    
-def is_point_in_trapezoid(point, trapezoid_points):
-    return cv2.pointPolygonTest(trapezoid_points, point, False) >= 0
-    
-def plot_bboxes(image, boxes, b1, b2, trapezoid_points, labels=[], colors=[], score=True, conf=None):
-  #Define COCO Labels
-  global cb # box center 
-  global cnt
-  if labels == []:
-    labels = {0: u'__background__', 1: u'person'}
-  #Define colors
-  if colors == []:
-    #colors = [(6, 112, 83), (253, 246, 160), (40, 132, 70), (205, 97, 162), (149, 196, 30), (106, 19, 161), (127, 175, 225), (115, 133, 176), (83, 156, 8), (182, 29, 77), (180, 11, 251), (31, 12, 123), (23, 6, 115), (167, 34, 31), (176, 216, 69), (110, 229, 222), (72, 183, 159), (90, 168, 209), (195, 4, 209), (135, 236, 21), (62, 209, 199), (87, 1, 70), (75, 40, 168), (121, 90, 126), (11, 86, 86), (40, 218, 53), (234, 76, 20), (129, 174, 192), (13, 18, 254), (45, 183, 149), (77, 234, 120), (182, 83, 207), (172, 138, 252), (201, 7, 159), (147, 240, 17), (134, 19, 233), (202, 61, 206), (177, 253, 26), (10, 139, 17), (130, 148, 106), (174, 197, 128), (106, 59, 168), (124, 180, 83), (78, 169, 4), (26, 79, 176), (185, 149, 150), (165, 253, 206), (220, 87, 0), (72, 22, 226), (64, 174, 4), (245, 131, 96), (35, 217, 142), (89, 86, 32), (80, 56, 196), (222, 136, 159), (145, 6, 219), (143, 132, 162), (175, 97, 221), (72, 3, 79), (196, 184, 237), (18, 210, 116), (8, 185, 81), (99, 181, 254), (9, 127, 123), (140, 94, 215), (39, 229, 121), (230, 51, 96), (84, 225, 33), (218, 202, 139), (129, 223, 182), (167, 46, 157), (15, 252, 5), (128, 103, 203), (197, 223, 199), (19, 238, 181), (64, 142, 167), (12, 203, 242), (69, 21, 41), (177, 184, 2), (35, 97, 56), (241, 22, 161)]
-    colors = [(89, 161, 197),(67, 161, 255),(19, 222, 24),(186, 55, 2),(167, 146, 11),(190, 76, 98),(130, 172, 179),(115, 209, 128),(204, 79, 135),(136, 126, 185),(209, 213, 45),(44, 52, 10),(101, 158, 121),(179, 124, 12),(25, 33, 189),(45, 115, 11),(73, 197, 184),(62, 225, 221),(32, 46, 52),(20, 165, 16),(54, 15, 57),(12, 150, 9),(10, 46, 99),(94, 89, 46),(48, 37, 106),(42, 10, 96),(7, 164, 128),(98, 213, 120),(40, 5, 219),(54, 25, 150),(251, 74, 172),(0, 236, 196),(21, 104, 190),(226, 74, 232),(120, 67, 25),(191, 106, 197),(8, 15, 134),(21, 2, 1),(142, 63, 109),(133, 148, 146),(187, 77, 253),(155, 22, 122),(218, 130, 77),(164, 102, 79),(43, 152, 125),(185, 124, 151),(95, 159, 238),(128, 89, 85),(228, 6, 60),(6, 41, 210),(11, 1, 133),(30, 96, 58),(230, 136, 109),(126, 45, 174),(164, 63, 165),(32, 111, 29),(232, 40, 70),(55, 31, 198),(148, 211, 129),(10, 186, 211),(181, 201, 94),(55, 35, 92),(129, 140, 233),(70, 250, 116),(61, 209, 152),(216, 21, 138),(100, 0, 176),(3, 42, 70),(151, 13, 44),(216, 102, 88),(125, 216, 93),(171, 236, 47),(253, 127, 103),(205, 137, 244),(193, 137, 224),(36, 152, 214),(17, 50, 238),(154, 165, 67),(114, 129, 60),(119, 24, 48),(73, 8, 110)]
-
-  #plot each boxes
-  for box in boxes:
-    #add score in label if score=True
-    if int(box[-1]) == 0:
-      if score :
-        label = labels[int(box[-1])+1] + " " + str(round(100 * float(box[-2]),1)) + "%"
-      else :
-        label = labels[int(box[-1])+1]
-        #print(box[-2])
-      #filter every box under conf threshold if conf threshold setted
-      if conf :
-        if box[-2] > conf:
-          color = colors[int(box[-1])]
-          box_label(image, box, b1, b2, label, color)
-          p1 = (int(box[0]), int(box[1]))
-          p2 = (int(box[2]), int(box[3]))
-          p3 = (int(box[0]), int(box[3]))
-          
-          points_to_check = [p1, p2, (p1[0], p2[1]), (p2[0], p1[1])]
-          for point in points_to_check:
-              if is_point_in_trapezoid(point, trapezoid_points):
-                # filename = f"./images/{cnt}_move.jpg"
-                # cnt += 1
-                # cv2.imwrite(filename, image)
-                # print(f"STOOOOOOOOP")
-                return image, f"STOP"
-      else:
-        color = colors[int(box[-1])]
-        box_label(image, box, b1, b2, label, color)
-        
-            # Check if any point in the boxes lies within the trapezoid
-  # filename = f"./images/{cnt}_move.jpg"
-  # cnt += 1
-# plt.show()
-
-  return image, "MOVE"
-
-cf =  320
-
-model = YOLO("yolov8n.pt")
-
-# #path = './soccer_pics/frame_30.jpg'
-# path = './A/a.jpg'
-
-# image = Image.open(path)
-# image = np.asarray(image)
-
-# image = cv2.resize(image, (640, 480))
-
-# current_result = model.predict(image)
-
-# boxes = current_result[0].boxes.data
-# #print("prev boxes", len(boxes))
-
-# # plotting boxes in original image and saving the coordinates   
-
-# black1 = np.zeros_like(image, dtype=np.uint8)
-# b1 = []
-# b2 = []
-# # Define trapezoid points
-# trapezoid_points = np.array([[140, 480], [500, 480], [370, 280], [270, 280]], np.int32)
-# trapezoid_points = trapezoid_points.reshape((-1, 1, 2))
-# cv2.polylines(image, [trapezoid_points], isClosed=True, color=(0, 0, 255), thickness=2)
-
-# image = plot_bboxes(image, boxes, b1, b2, trapezoid_points, score=True, conf=0.90)
-
-# # plt.imshow(image)
-# # plt.show()
-
-
-# ----- COMMENTED OUT ER 08/19/2024 - 09:06 AM ---------------------------------------------------
-# def detect_object(msg):
-#   image = cv2.resize(msg, (640, 480))
-
-#   current_result = model.predict(image)
-
-#   boxes = current_result[0].boxes.data
-#   #print("prev boxes", len(boxes))
-
-#   # plotting boxes in original image and saving the coordinates   
-
-#   black1 = np.zeros_like(image, dtype=np.uint8)
-#   b1 = []
-#   b2 = []
-#   # Define trapezoid points
-#   trapezoid_points = np.array([[140, 480], [500, 480], [370, 280], [270, 280]], np.int32)
-#   trapezoid_points = trapezoid_points.reshape((-1, 1, 2))
-#   cv2.polylines(image, [trapezoid_points], isClosed=True, color=(0, 0, 255), thickness=2)
-
-#   image, result = plot_bboxes(image, boxes, b1, b2, trapezoid_points, score=True, conf=0.50)
-
-#   return image, result
-#   # cv2.imwrite(filename, image)
-#   # cv2.imshow(image)
-# # plt.show()
-
-#   return image, "MOVE"
-#-------------------------------------------------------------------------------------------------
-# cf =  320
-
-# model = YOLO("yolov8n.pt")
-
-# #path = './soccer_pics/frame_30.jpg'
-# path = './A/a.jpg'
-
-# image = Image.open(path)
-# image = np.asarray(image)
-
-# image = cv2.resize(image, (640, 480))
-
-# current_result = model.predict(image)
-
-# boxes = current_result[0].boxes.data
-# #print("prev boxes", len(boxes))
-
-# # plotting boxes in original image and saving the coordinates   
-
-# black1 = np.zeros_like(image, dtype=np.uint8)
-# b1 = []
-# b2 = []
-# # Define trapezoid points
-# trapezoid_points = np.array([[140, 480], [500, 480], [370, 280], [270, 280]], np.int32)
-# trapezoid_points = trapezoid_points.reshape((-1, 1, 2))
-# cv2.polylines(image, [trapezoid_points], isClosed=True, color=(0, 0, 255), thickness=2)
-
-# image = plot_bboxes(image, boxes, b1, b2, trapezoid_points, score=True, conf=0.90)
-
-# # plt.imshow(image)
-# # plt.show()
-
-
-
-# def detect_object(msg):
-#   image = cv2.resize(msg, (640, 480))
-
-#   current_result = model.predict(image)
-
-#   boxes = current_result[0].boxes.data
-#   #print("prev boxes", len(boxes))
-
-#   # plotting boxes in original image and saving the coordinates   
-
-#   black1 = np.zeros_like(image, dtype=np.uint8)
-#   b1 = []
-#   b2 = []
-#   # Define trapezoid points
-#   trapezoid_points = np.array([[140, 480], [500, 480], [370, 280], [270, 280]], np.int32)
-#   trapezoid_points = trapezoid_points.reshape((-1, 1, 2))
-#   cv2.polylines(image, [trapezoid_points], isClosed=True, color=(0, 0, 255), thickness=2)
-
-#   image, result = plot_bboxes(image, boxes, b1, b2, trapezoid_points, score=True, conf=0.50)
-
-#   return image, result
-
-
-
-
-
-##########################NEW VERSION###################################
-from ultralytics import YOLO
-import numpy as np
-from PIL import Image
-import requests
-from io import BytesIO
-import cv2
-from matplotlib import pyplot as plt
-import glob
-
 def box_label(image, box, b1, b2, label='', color=(128, 128, 128), txt_color=(255, 255, 255)):
   lw = max(round(sum(image.shape) / 2 * 0.003), 2)
   p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
@@ -272,7 +79,7 @@ def detect_object(msg):
   b2 = []
 
   # Define trapezoid points
-  trapezoid_points = np.array([[140, 480], [500, 480], [370, 280], [270, 280]], np.int32)
+  trapezoid_points = np.array([[140, 480], [530, 480], [370, 280], [270, 280]], np.int32)
   trapezoid_points = trapezoid_points.reshape((-1, 1, 2))
   cv2.polylines(image, [trapezoid_points], isClosed=True, color=(0, 0, 255), thickness=2)
 
@@ -284,7 +91,7 @@ def detect_object(msg):
     person = True
     if p2[1] > 280 and cb > cf and p1[0] < 530:
         direction = f"left"
-    elif p2[1] > 280 and cb < cf and p2[0] > 110:
+    elif p2[1] > 280 and cb < cf and p2[0] > 140:
       direction = f"right"
     else:
       direction = f"straight"
@@ -292,5 +99,4 @@ def detect_object(msg):
 
   else:
     direction = f"straight"
-  
-  return image, direction
+  return image, direction, person
--- workspaces/isaac_ros-dev/src/object_avoidance/object_avoidance/object_avoidance_node.py
+++ workspaces/isaac_ros-dev/src/object_avoidance/object_avoidance/object_avoidance_node.py
@@ -14,7 +14,8 @@ class ObjectAvoidanceNode(Node):
         super().__init__("object_avoid")
         
         self.is_turning = False
-        self.turing_path = []
+        self.turning_path = []
+        self.turning_path_B = []
         self.logger = self.get_logger()
         self.bridge = CvBridge()
 
@@ -22,57 +23,161 @@ class ObjectAvoidanceNode(Node):
         self.video_publisher = self.create_publisher(Image, '/video/object_detection', 1)
 
         self.front_camera_subscription = self.create_subscription(Image, '/video/front_camera', self.front_camera_callback, 1)
-        
+
     def front_camera_callback(self, msg):
         speed = 0.2
-        self.get_logger().info('front_camera_callback')
+        angle = 0.0
+        # self.get_logger().info('front_camera_callback')
         try:
             image_array = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-            image, result = detect_object(image_array)
-
+            image, direction, person = detect_object(image_array)   
             video_frame = self.bridge.cv2_to_imgmsg(image, 'bgr8')
             self.video_publisher.publish(video_frame)
-                
-            if result == 'left':
+         
+            # if statement that checks if person is false, self.is_turning is false, and the length of self.turning_path and self.turning_path_B is = 0
+            if direction == 'straight' and self.is_turning == False and len(self.turning_path) == 0 and len(self.turning_path_B) == 0:
+                angle = 0.0
+                self.get_logger().info(f"STAG 0: GO STRAIGHT")  
+               
+            elif direction == 'left':
                 self.is_turning = True
                 angle = 0.7
-                
-            elif result == 'right':
+                self.get_logger().info(f"STAG 1: LEFT | CNT: {len(self.turning_path)}") 
+               
+            elif direction == 'right':
                 self.is_turning = True
-                angle = -0.7 
+                angle = -0.7
+                self.get_logger().info(f"STAG 1: RIGHT | CNT: {len(self.turning_path)}") 
             
-            else:
-                self.is_turning = False
-                angle = 0.0 
-                
-            if self.is_turning == False and len(self.turing_path) > 0:
-                self.reverse_angle()
-                return
-            
-            self.get_logger().info(f"direction: {result}")
+            # elif statement that checks if direction is straight, self.is_turning is true, and the length of self.turning_path and self.turning_path_B is > 0
+            elif direction == 'straight' and self.is_turning == True and len(self.turning_path_B) > 0: 
+                self.get_logger().info(f"STAG 2: REVERSE | CNT: {len(self.turning_path)}") 
+                if len(self.turning_path) > 0:
+                    self.reverse_angle(-1)
+                    return
+                elif len(self.turning_path) == 0:
+                    self.get_logger().info(f"STAG 2: REVERSE END") 
+                    angle = 0.0
+                    self.is_turning = False
 
-            twist = geometry_msgs.msg.Twist()
-            twist.linear.x = speed
-            twist.linear.y = 0.0
-            twist.linear.z = 0.0
-            twist.angular.x = 0.0
-            twist.angular.y = 0.0
-            twist.angular.z = angle
+            elif direction == 'straight' and self.is_turning == False and person == True:
+                self.get_logger().info(f"STAG 3: GO STRAIGHT") 
+                angle = 0.0
+
+            elif direction == 'straight' and self.is_turning == False and person == False and len(self.turning_path) == 0 and len(self.turning_path_B) > 0:
+                # self.get_logger().info(f"STAG 3: COPY TURNING STACK 2 TIMES") 
+                for twist in self.turning_path_B:
+                    twist = self.copy_twist(twist)
+                    twist.angular.z = -twist.angular.z
+                    self.turning_path.append(twist)
+                
+                for twist in self.turning_path_B:
+                    twist = self.copy_twist(twist)
+                    self.turning_path.append(twist)
+                self.get_logger().info(f"STAG 3: COPY TURNING STACK 2 TIMES | CNT: {len(self.turning_path)}") 
+                self.turning_path_B = []
+                angle = 0.0 
             
+            elif direction == 'straight' and self.is_turning == False and person == False and len(self.turning_path) > 0 and len(self.turning_path_B) == 0:
+                self.get_logger().info(f"STAG 4: REVERSE 2 TIMES | CNT: {len(self.turning_path)}") 
+                self.reverse_angle(1)
+                return    
+             
+            # self.get_logger().info(f"direction: {direction}")   
+            twist = self.create_twist(speed, angle)
+           
             self.front_publisher_.publish(twist)
-            
-            if self.is_turning:
-                self.turing_path.append(twist)
-            
-        
+            self.get_logger().info(f'FCP Twist: {twist}')
+           
+            if self.is_turning and direction in ['left', 'right']:
+                self.turning_path.append(twist)
+                self.turning_path_B.append(twist)
+           
+    
         except Exception as e:
             self.get_logger().error(f"Error in front_camera_callback: {e}")
-            
-    def reverse_angle(self):
-        twist = self.turing_path.pop()
-        twist.angular.z = -twist.angular.z
-        self.get_logger().info(f"reverse: {twist}")
+           
+    def reverse_angle(self,a):
+        twist = self.turning_path.pop()
+        twist.angular.z = a*twist.angular.z
+        # self.get_logger().info(f"reverse: {twist}")
         self.front_publisher_.publish(twist)
+        self.get_logger().info(f'FCP Twist: {twist}')
+        return twist
+    
+    def copy_twist(self, twist):
+        return self.create_twist(twist.linear.x, twist.angular.z)
+
+    def create_twist(self, speed, angle):
+        twist = geometry_msgs.msg.Twist()
+        twist.linear.x = speed
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = angle
+        return twist
+
+        
+    # def reverse_angle2(self,a):
+    #     twist = self.turning_path_B.pop()
+    #     twist.angular.z = a*twist.angular.z
+    #     self.get_logger().info(f"reverse: {twist}")
+    #     self.front_publisher_.publish(twist) 
+    #     return twist 
+        
+    # def front_camera_callback(self, msg):
+    #     speed = 0.2
+    #     self.get_logger().info('front_camera_callback')
+    #     try:
+    #         image_array = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+    #         image, result = detect_object(image_array)
+
+    #         video_frame = self.bridge.cv2_to_imgmsg(image, 'bgr8')
+    #         self.video_publisher.publish(video_frame)
+
+
+                
+    #         if result == 'left':
+    #             self.is_turning = True
+    #             angle = 0.7
+                
+    #         elif result == 'right':
+    #             self.is_turning = True
+    #             angle = -0.7 
+            
+    #         else:
+    #             self.is_turning = False
+    #             angle = 0.0 
+                
+    #         if self.is_turning == False and len(self.turning_path) > 0:
+    #             self.reverse_angle()
+    #             return
+            
+    #         self.get_logger().info(f"direction: {result}")
+
+    #         twist = geometry_msgs.msg.Twist()
+    #         twist.linear.x = speed
+    #         twist.linear.y = 0.0
+    #         twist.linear.z = 0.0
+    #         twist.angular.x = 0.0
+    #         twist.angular.y = 0.0
+    #         twist.angular.z = angle
+            
+    #         self.front_publisher_.publish(twist)
+            
+    #         if self.is_turning:
+    #             self.turning_path.append(twist)
+            
+        
+    #     except Exception as e:
+    #         self.get_logger().error(f"Error in front_camera_callback: {e}")
+            
+    # def reverse_angle(self):
+    #     twist = self.turning_path.pop()
+    #     twist.angular.z = -twist.angular.z
+    #     self.get_logger().info(f"reverse: {twist}")
+    #     self.front_publisher_.publish(twist)
 
 
 def main(args=None):
