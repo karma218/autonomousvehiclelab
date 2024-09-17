9009d1c37bf8f3ac5ab4e64d15a47f262d292604
agxorin1 <agxorin1@agxorin1>
2024-09-17 10:26:42 -0700

    cleaning up repo

--- workspaces/isaac_ros-dev/src/build/gps_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/agxorin1/autonomousvehiclelab/workspaces/isaac_ros-dev/src/install/gps_package'
