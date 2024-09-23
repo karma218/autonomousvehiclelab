99a86f4b82bdd3337f75a3807819490305e7df4b
agxorin1 <agxorin1@agxorin1>
2024-09-23 12:55:04 -0700

    test

--- isaac_ros-dev/src/teleop_twist_keyboard/teleop_twist_keyboard.py
+++ isaac_ros-dev/src/teleop_twist_keyboard/teleop_twist_keyboard.py
@@ -1,38 +1,5 @@
-# Copyright 2011 Brown University Robotics.
-# Copyright 2017 Open Source Robotics Foundation, Inc.
-# All rights reserved.
-#
-# Software License Agreement (BSD License 2.0)
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of the Willow Garage nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
 import sys
-
+import select
 import geometry_msgs.msg
 import rclpy
 
@@ -63,12 +30,12 @@ CTRL-C to quit
 """
 
 moveBindings = {
-    'w': (1, 0, 0, 0),
-    'a': (0, 0, 0, 1),
-    's': (-1, 0, 0, 0),
-    'd': (0, 0, 0, -1),
-    't': (0, 0, 1, 0),
-    'b': (0, 0, -1, 0),
+    'w': (1, 0, 0, 0),     # Move forward
+    'a': (0, 0, 0, 1),     # Turn left
+    's': (-1, 0, 0, 0),    # Move backward
+    'd': (0, 0, 0, -1),    # Turn right
+    't': (0, 0, 1, 0),     # Move up
+    'b': (0, 0, -1, 0),    # Move down
 }
 
 # Use arrow keys for speed adjustment
@@ -80,15 +47,21 @@ speedBindings = {
 
 def getKey(settings):
     if sys.platform == 'win32':
-        # getwch() returns a string on Windows
-        key = msvcrt.getwch()
+        # Check if a key is pressed on Windows
+        if msvcrt.kbhit():
+            return msvcrt.getwch()
+        return ''
     else:
         tty.setraw(sys.stdin.fileno())
-        key = sys.stdin.read(1)
-        if key == '\x1b':  # Check for escape sequences (arrow keys)
-            key += sys.stdin.read(2)
+        rlist, _, _ = select.select([sys.stdin], [], [], 0.1)  # Non-blocking input
+        if rlist:
+            key = sys.stdin.read(1)
+            if key == '\x1b':  # Check for escape sequences (arrow keys)
+                key += sys.stdin.read(2)
+        else:
+            key = ''
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
+        return key
 
 
 def saveTerminalSettings():
@@ -117,44 +90,50 @@ def main():
 
     speed = 0.3
     turn = 1.0
-    x = 0.0
-    y = 0.0
-    z = 0.0
-    th = 0.0
     status = 0.0
+    active_keys = set()  # Track pressed keys
 
     try:
         print(msg)
         print(vels(speed, turn))
         while True:
             key = getKey(settings)
-            if key in moveBindings.keys():
-                x = moveBindings[key][0]
-                y = moveBindings[key][1]
-                z = moveBindings[key][2]
-                th = moveBindings[key][3]
-            elif key in speedBindings.keys():
+            
+            if key == '\x03':  # CTRL+C to quit
+                break
+
+            # Add key to active keys if valid for movement
+            if key in moveBindings:
+                active_keys.add(key)
+
+            # Handle speed adjustment
+            elif key in speedBindings:
                 speed = speed * speedBindings[key][0]
                 print(vels(speed, turn))
-                if (status == 14):
+                if status == 14:
                     print(msg)
                 status = (status + 1) % 15
-            else:
-                x = 0.0
-                y = 0.0
-                z = 0.0
-                th = 0.0
-                if (key == '\x03'):
-                    break
 
+            # Handle key release for move keys
+            if key == '':  # No key pressed, stop movement
+                active_keys.clear()
+
+            # Calculate movement by summing over active keys
+            x, y, z, th = 0, 0, 0, 0
+            for k in active_keys:
+                dx, dy, dz, dth = moveBindings[k]
+                x += dx
+                y += dy
+                z += dz
+                th += dth
+
+            # Publish the movement message
             twist = geometry_msgs.msg.Twist()
             twist.linear.x = x * speed
             twist.linear.y = y * speed
             twist.linear.z = z * speed
-            twist.angular.x = 0.0
-            twist.angular.y = 0.0
             twist.angular.z = th * turn
-            print(f'key pressed: {key},', vels(speed, turn))
+            print(f'movement: {twist.linear.x}, {twist.linear.y}, {twist.linear.z}, {twist.angular.z}')
             pub.publish(twist)
 
     except Exception as e:
