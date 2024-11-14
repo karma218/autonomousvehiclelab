2dd263c947614eb27d273c7796cbc472671761c6
agxorin1 <agxorin1@agxorin1>
2024-11-14 14:29:13 -0800

    initial changes for running yolopv2 perception

---
+++ isaac_ros-dev/src/yolopv2_perception/run_perception.py
@@ -0,0 +1,158 @@
+import argparse
+import time
+from pathlib import Path
+import cv2
+import torch
+
+# Conclude setting / general reprocessing / plots / metrices / datasets
+from utils.utils import \
+    time_synchronized,select_device, increment_path,\
+    scale_coords,xyxy2xywh,non_max_suppression,split_for_trace_model,\
+    driving_area_mask,lane_line_mask,plot_one_box,show_seg_result,\
+    AverageMeter,\
+    LoadImages
+
+class Percept(object):
+        def __init__(self, opt):
+            self.model =  torch.jit.load(opt.weights)
+            self.source = opt.source
+            self.device = select_device(opt.device)
+            self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
+            self.model = self.model.to(self.device)
+            if self.half:
+                self.model.half()  # to FP16  
+            self.model.eval()
+
+
+        def make_parser():
+            parser = argparse.ArgumentParser()
+            parser.add_argument('--weights', nargs='+', type=str, default='data/weights/yolopv2.pt', help='model.pt path(s)')
+            parser.add_argument('--source', type=str, default='data/example.jpg', help='source')  # file/folder, 0 for webcam
+            parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+            parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
+            parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+            parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+            parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+            parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+            parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+            parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+            parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+            parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+            parser.add_argument('--name', default='exp', help='save results to project/name')
+            parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+            return parser
+
+
+        def detect(self, img):
+            # setting and directories
+            source, weights,  save_txt, imgsz = opt.source, opt.weights,  opt.save_txt, opt.img_size
+            save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+
+            
+
+            inf_time = AverageMeter()
+            waste_time = AverageMeter()
+            nms_time = AverageMeter()
+
+            # Load model
+            stride =32
+    
+            # Set Dataloader
+            vid_path, vid_writer = None, None
+
+            # Run inference
+            if self.device.type != 'cpu':
+                print("Here!")
+                self.model(torch.zeros(1, 3, imgsz, imgsz).to(self.device).type_as(next(self.model.parameters())))  # run once
+            t0 = time.time()
+            img = torch.from_numpy(img).to(self.device)
+            img = img.half() if self.half else img.float()  # uint8 to fp16/32
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+
+            # Inference
+            t1 = time_synchronized()
+            [pred,anchor_grid],seg,ll= self.model(img)
+            t2 = time_synchronized()
+
+            # waste time: the incompatibility of  torch.jit.trace causes extra time consumption in demo version 
+            # but this problem will not appear in offical version 
+            tw1 = time_synchronized()
+            pred = split_for_trace_model(pred,anchor_grid)
+            tw2 = time_synchronized()
+
+            # Apply NMS
+            t3 = time_synchronized()
+            pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+            t4 = time_synchronized()
+
+            da_seg_mask = driving_area_mask(seg)
+            ll_seg_mask = lane_line_mask(ll)
+
+            # Process detections
+            for i, det in enumerate(pred):  # detections per image
+                
+                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+                s += '%gx%g ' % img.shape[2:]  # print string
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                    # Print results
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()  # detections per class
+                        #s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        if save_txt:  # Write to file
+                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                            line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                            with open(txt_path + '.txt', 'a') as f:
+                                f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                        if save_img :  # Add bbox to image
+                            plot_one_box(xyxy, im0, line_thickness=3)
+
+                # Print time (inference)
+                print(f'{s}Done. ({t2 - t1:.3f}s)')
+                show_seg_result(im0, (da_seg_mask,ll_seg_mask), is_demo=True)
+
+                # Save results (image with detections)
+                if save_img:
+                    if dataset.mode == 'image':
+                        cv2.imwrite(save_path, im0)
+                        print(f" The image with the result is saved in: {save_path}")
+                    else:  # 'video' or 'stream'
+                        if vid_path != save_path:  # new video
+                            vid_path = save_path
+                            if isinstance(vid_writer, cv2.VideoWriter):
+                                vid_writer.release()  # release previous video writer
+                            if vid_cap:  # video
+                                fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                                #w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                                #h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                                w,h = im0.shape[1], im0.shape[0]
+                            else:  # stream
+                                fps, w, h = 30, im0.shape[1], im0.shape[0]
+                                save_path += '.mp4'
+                            vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                        vid_writer.write(im0)
+
+        inf_time.update(t2-t1,img.size(0))
+        nms_time.update(t4-t3,img.size(0))
+        waste_time.update(tw2-tw1,img.size(0))
+        print('inf : (%.4fs/frame)   nms : (%.4fs/frame)' % (inf_time.avg,nms_time.avg))
+        print(f'Done. ({time.time() - t0:.3f}s)')
+
+
+if __name__ == '__main__':
+opt =  make_parser().parse_args()
+print(opt)
+
+with torch.no_grad():
+        detect()
