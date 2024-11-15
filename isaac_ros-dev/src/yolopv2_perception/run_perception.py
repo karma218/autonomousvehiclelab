b2c0b687fd850eb87bc0b9d0f94ba7c19ea65883
agxorin1 <agxorin1@agxorin1>
2024-11-14 16:20:53 -0800

    mino change

--- isaac_ros-dev/src/yolopv2_perception/run_perception.py
+++ isaac_ros-dev/src/yolopv2_perception/run_perception.py
@@ -106,6 +106,12 @@ class Percept(object):
                     for c in det[:, -1].unique():
                         n = (det[:, -1] == c).sum()  # detections per class
                         #s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                    
+                    # if save_txt:  # Write to file
+                    #     xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    #     line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                    #     with open(txt_path + '.txt', 'a') as f:
+                    #         f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
                     # Write results
                     for *xyxy, conf, cls in reversed(det):
