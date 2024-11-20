a5a866505af6ce9bb04a2e588b83f0b561e96465
Conspiciens <fernandezjohann08@gmail.com>
2024-11-20 01:26:11 -0800

    Flask server and control page

--- control_front_end/my-control-car/src/app/control/page.tsx
+++ control_front_end/my-control-car/src/app/control/page.tsx
@@ -1,11 +1,15 @@
+'use client'
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from 'next/link'
 import { redirect } from 'next/navigation';
 
 
 export default function carControls() {
+    const [isData, setData] = useState(null);
+    
+
     const sendKeyPressToRos = (event: any) => {
        if (event.key === "w"){
 
@@ -21,7 +25,7 @@ export default function carControls() {
     return (
         <>
             <div> 
-                
+                <img src="http://127.0.0.1:5000/" /> 
             </div>
         </>
     );
