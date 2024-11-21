c1495ae93d8025004c8580e54c719c7deddd8402
agxorin1 <agxorin1@agxorin1>
2024-11-21 13:31:22 -0800

    added server for web control

--- flask_server/heartbeat.py
+++ flask_server/heartbeat.py
@@ -1,37 +1,16 @@
-from flask import Flask, render_template, Response, request, jsonify 
+from flask import Flask, jsonify
 from flask_cors import CORS
-from aiortc import RTCPeerConnection, RTCSessionDescription 
-from datetime import datetime, timedelta
-import threading
-import cv2 
-import json 
-import uuid
-import asyncio
-import logging 
+from datetime import datetime, timedelta 
+import json
 import time 
+import threading 
 
 app = Flask(__name__, static_url_path='/static') 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-vehicle_status = {"last_heartbeat": None, "is_alive": False}
+vehicle_status = {"last_heartbeat": None, "is_alive": False} 
 HEARTBEAT_TIMEOUT = 5
 
-def generate_frames():
-    camera = cv2.VideoCapture(0) 
-    
-    while True: 
-        start_time = time.time()
-        success, frame = camera.read()
-        if not success: 
-            break
-            
-
-        ret, buffer = cv2.imencode('.jpg', frame) 
-        frame = buffer.tobytes() 
-        yield(b'--frame\r\n'
-            b'Content-type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        elasped_time = time.time() - start_time
-        
 
 def monitor_heartbeat(): 
     while True: 
@@ -44,10 +23,7 @@ def monitor_heartbeat():
             vehicle_status["is_alive"] = True
         threading.Event().wait(1) 
     
- 
-@app.route('/')
-def video_feed(): 
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame') 
+
 
 @app.route('/heartbeat') 
 def heartbeat_check():
@@ -55,10 +31,10 @@ def heartbeat_check():
         "is_alive": vehicle_status["is_alive"],
         "last_heartbeat": vehicle_status["last_heartbeat"] 
     }) 
-     
+
 
 if __name__ == '__main__':
     heartbeat_thread = threading.Thread(target=monitor_heartbeat, daemon=True)
     heartbeat_thread.start() 
 
-    app.run(debug=True)
+    app.run(debug=True, host="10.110.194.54")
