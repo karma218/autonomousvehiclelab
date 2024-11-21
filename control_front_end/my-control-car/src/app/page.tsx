ba6db389197b4d4f48b01f68f3ff0e70fd693ca1
Conspiciens <fernandezjohann08@gmail.com>
2024-11-20 17:55:52 -0800

    Added .gitignore and checking car heartbeat

--- control_front_end/my-control-car/src/app/page.tsx
+++ control_front_end/my-control-car/src/app/page.tsx
@@ -8,16 +8,30 @@ import { redirect } from 'next/navigation';
 
 
 export default function Home() {
+  const [isAlive, setAlive] = useState(false);
   const [isLoading, setLoading] = useState(false);
   
-  const connectToAPI = () => {
+  const checkCarHeartBeat = async () => {
     setLoading(true);
+    try {
+      const req = await fetch("http://127.0.0.1:5000/heartbeat"); 
+      if (!req.ok){
+        throw new Error(`Error: ${req.status}`);
+      }
 
+      const data = await req.json(); 
+
+      if (data["is_alive"] !== false) {
+        setAlive(true);
+        redirect('/control');
+      }
+
+      setAlive(false);
+    } catch(error) {
+      setLoading(false);
+    };
   }
 
-  const testNav = () => {
-    redirect('/control');
-  }
   
   return (
   <>
@@ -26,21 +40,19 @@ export default function Home() {
         "Welcome to Cal Poly Pomona's Car Controls!!!" 
       </div>
 
-      <Link href="/control"> 
-      <Button className="check-if-active" variant="primary" onClick={testNav}>
-        {isLoading == true ? <Spinner
-          animation="border" 
-          role="status"
-          size="sm"
-          aria-hidden="true">
-        </Spinner> :
-          <div></div>
-        }
-        <span className={isLoading === true ? "visuallyHidden" : "visuallyVisible" }>
-          {isLoading === true ? "Connecting..." : "Connect"}
-        </span>
-      </Button>
-      </Link>
+        <Button className="check-if-active" variant="primary" onClick={checkCarHeartBeat}>
+          {isLoading == true ? <Spinner
+            animation="border" 
+            role="status"
+            size="sm"
+            aria-hidden="true">
+          </Spinner> :
+            <div></div>
+          }
+          <span className={isLoading === true ? "visuallyHidden" : "visuallyVisible" }>
+            {isLoading === true ? "Connecting..." : "Connect"}
+          </span>
+        </Button>
     </div> 
   </>
   );
