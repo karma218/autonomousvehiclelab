b79b93938a3f5bf7b9094cd22c1ad88f76cefd65
Conspiciens <fernandezjohann08@gmail.com>
2024-11-21 17:36:30 -0800

    Added websocket to connect to car

--- control_front_end/my-control-car/src/app/control/page.tsx
+++ control_front_end/my-control-car/src/app/control/page.tsx
@@ -1,31 +1,73 @@
 'use client'
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from 'next/link'
 import { redirect } from 'next/navigation';
-
+import { ChevronCompactDown, ChevronCompactLeft, ChevronCompactRight, ChevronBarUp } from 'react-bootstrap-icons';
+import {io, Socket} from 'socket.io-client';
+import "./controlPage.css";
+import { useFormState } from 'react-dom';
 
 export default function carControls() {
+    let socket = useRef<Socket | null> (null); 
     const [isData, setData] = useState(null);
-    
 
-    const sendKeyPressToRos = (event: any) => {
-       if (event.key === "w"){
 
+    const [isLeft, setLeft] = useState(false);
+    const [isRight, setRight] = useState(false);
+    const [isForward, setForward] = useState(false);
+    const [isBackward, setBackward] = useState(false);
+
+    socket.current = io("http://10.110.194.54:5002", {
+        transports: ["websocket"], 
+    });
+
+    useEffect(() => {
+        socket.current!.on('connect', () => {
+            console.log("Conneted");
+            console.log(socket.current);
+        })
+        
+        socket.current!.on("disconnect", () => {
+            console.log(socket.current!.id); // undefined
+          });
+
+        socket.current!.on("connect_error", (err) => {
+            console.log(err.message);
+        
+          })
+    }, [])
+
+    const sendKeyPressToRos = async(event: any) => {
+        if (event.key === "w"){
+            socket.current!.emit('keypress', {key: event.key, action: "press"}, () => {
+                console.log("sent");
+            });
        } else if (event.key === "s") {
-
-       } else if (event.key === "d") {
-
-       } else if (event.key === "a"){
-
+            socket.current!.emit('keypress', {key: event.key, action: "press"}, () => {
+                console.log("sent");
+            });
+       } else if (event.key === "a") {
+            socket.current!.emit('keypress', {key: event.key, action: "press"}, () => {
+                console.log("sent");
+            });
+       } else if (event.key === "d"){
+            socket.current!.emit('keypress', {key: event.key, action: "press"}, () => {
+                console.log("sent");
+            });
        }
     }
 
     return (
         <>
-            <div> 
-                <img src="http://127.0.0.1:5000/" /> 
+            <div className="allStreamControls" onKeyDown={sendKeyPressToRos} tabIndex={0}> 
+                <div className="videoStream" tabIndex={0}> 
+                    <img className="videoFrame" src="http://10.110.194.54:5002/camera/stream" /> 
+                </div>
+                <div className="controls" tabIndex={0}>
+                    <ChevronCompactDown size={20}></ChevronCompactDown>
+                </div>
             </div>
         </>
     );
--- control_front_end/my-control-car/src/app/page.tsx
+++ control_front_end/my-control-car/src/app/page.tsx
@@ -1,20 +1,28 @@
 "use client"
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
-import { useState } from "react";
-import Link from 'next/link'
-import "./connectPage.css";
+import { useEffect, useState } from "react";
 import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { Check2Circle, XLg } from 'react-bootstrap-icons';
+import "../../public/cool_car-removebg-preview.png";
+import "./connectPage.css";
 
 
 export default function Home() {
+  const router = useRouter()
+
   const [isAlive, setAlive] = useState(false);
+  const [isLoadingInital, setLoadingInital] = useState(true);
   const [isLoading, setLoading] = useState(false);
-  
-  const checkCarHeartBeat = async () => {
-    setLoading(true);
+
+  useEffect(() => {
+    initalChecks();
+  }, []);
+
+  const initalChecks = async() => {
     try {
-      const req = await fetch("http://127.0.0.1:5000/heartbeat"); 
+      const req = await fetch("http://10.110.194.54:5000/heartbeat"); 
       if (!req.ok){
         throw new Error(`Error: ${req.status}`);
       }
@@ -23,24 +31,65 @@ export default function Home() {
 
       if (data["is_alive"] !== false) {
         setAlive(true);
-        redirect('/control');
+      }
+
+    } catch(error) {
+      setAlive(false);
+    };
+    setLoadingInital(false);
+  }
+  
+  const checkCarHeartBeat = async () => {
+    setLoading(true);
+    try {
+      const req = await fetch("http://10.110.194.54:5000/heartbeat"); 
+      if (!req.ok){
+        throw new Error(`Error: ${req.status}`);
+      }
+
+      const data = await req.json(); 
+
+      if (data["is_alive"] !== false) {
+        setAlive(true);
+        router.push('/control');
+        return;
       }
 
       setAlive(false);
     } catch(error) {
       setLoading(false);
     };
+    setLoading(false);
   }
 
   
   return (
   <>
     <div className="connectToScreen">
-      <div className="cuteMessage">
-        "Welcome to Cal Poly Pomona's Car Controls!!!" 
-      </div>
+      <img src="./cool_car-removebg-preview.png" className='carImg'/> 
+      <h1 className="cuteMessage">
+        Welcome to Cal Poly Pomona's Car Controls
+      </h1>
+        <ul className="checksForCar">
+          <li> 
+            Alive?
+            <span className="imgChecks">
+              &nbsp;
+              {isAlive === false && 
+               isLoadingInital === true && 
+               <Spinner animation='border' role='status' size='sm' aria-hidden="true" color="red" />}
 
-        <Button className="check-if-active" variant="primary" onClick={checkCarHeartBeat}>
+              {isAlive === true 
+                && isLoadingInital === false 
+                && <Check2Circle color="green" size={20} />} 
+
+              {isAlive === false 
+                && isLoadingInital === false 
+                && <XLg color="red" size={20}/>}
+            </span>
+          </li>
+        </ul>
+        <Button className="check-if-active" variant="primary" size="lg" onClick={checkCarHeartBeat}>
           {isLoading == true ? <Spinner
             animation="border" 
             role="status"
@@ -50,7 +99,7 @@ export default function Home() {
             <div></div>
           }
           <span className={isLoading === true ? "visuallyHidden" : "visuallyVisible" }>
-            {isLoading === true ? "Connecting..." : "Connect"}
+            {isLoading === true ? " Connecting..." : " Connect"}
           </span>
         </Button>
     </div> 
